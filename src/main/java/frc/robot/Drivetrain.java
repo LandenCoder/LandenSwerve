e70b2c55ee5df8213f2f7963cde06a2f6a5f31c2
Landen Geurink <jrgeurink19@gmail.com>
2025-08-05 19:19:55 -0400

    mini swerve drives, trying to get odometry field working

--- src/main/java/frc/robot/Drivetrain.java
+++ src/main/java/frc/robot/Drivetrain.java
@@ -4,67 +4,82 @@
 
 package frc.robot;
 
+import com.studica.frc.AHRS;
+import com.studica.frc.AHRS.NavXComType;
+
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.Odometry;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /** Represents a swerve drive style drivetrain. */
-public class Drivetrain {
+public class Drivetrain extends SubsystemBase {
   public static final double kMaxSpeed = 3.0; // 3 meters per second
   public static final double kMaxAngularSpeed = Math.PI; // 1/2 rotation per second
-  //TODO: measure into actual locations
+  // TODO: measure into actual locations
   private final Translation2d m_frontLeftLocation = new Translation2d(0.381, 0.381);
   private final Translation2d m_frontRightLocation = new Translation2d(0.381, -0.381);
   private final Translation2d m_backLeftLocation = new Translation2d(-0.381, 0.381);
   private final Translation2d m_backRightLocation = new Translation2d(-0.381, -0.381);
 
-  private final SwerveModule m_frontLeft = new SwerveModule(8, 1, 0);
-  private final SwerveModule m_frontRight = new SwerveModule(2, 3, 1);
-  private final SwerveModule m_backLeft = new SwerveModule(6, 7, 3);
-  private final SwerveModule m_backRight = new SwerveModule(4, 5, 2);
+  private final SwerveModule m_frontLeft = new SwerveModule(8, 1, 0, 0.34326, false, false);
+  private final SwerveModule m_frontRight = new SwerveModule(2, 3, 1, 0.29980, false, false);
+  private final SwerveModule m_backLeft = new SwerveModule(6, 7, 3, 0.26367, false, false);
+  private final SwerveModule m_backRight = new SwerveModule(4, 5, 2, 0.31396, false, false);
 
-  private final AnalogGyro m_gyro = new AnalogGyro(0);
+  private final SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(
+      m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation, m_backRightLocation);
 
-  private final SwerveDriveKinematics m_kinematics =
-      new SwerveDriveKinematics(
-          m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation, m_backRightLocation);
+  private final AHRS m_gyro;
 
-  private final SwerveDriveOdometry m_odometry =
-      new SwerveDriveOdometry(
-          m_kinematics,
-          m_gyro.getRotation2d(),
-          new SwerveModulePosition[] {
+  private final SwerveDriveOdometry m_odometry;
+  private Field2d m_field = new Field2d();
+
+  public Drivetrain() {
+    m_gyro = new AHRS(NavXComType.kUSB1);
+
+    m_gyro.reset();
+
+    SmartDashboard.putData("Field", m_field);
+
+    m_odometry = new SwerveDriveOdometry(
+        m_kinematics,
+        m_gyro.getRotation2d(),
+        new SwerveModulePosition[] {
             m_frontLeft.getPosition(),
             m_frontRight.getPosition(),
             m_backLeft.getPosition(),
             m_backRight.getPosition()
-          });
-
-  public Drivetrain() {
-    m_gyro.reset();
+        });
+    m_field.setRobotPose(m_odometry.getPoseMeters());
   }
 
   /**
    * Method to drive the robot using joystick info.
    *
-   * @param xSpeed Speed of the robot in the x direction (forward).
-   * @param ySpeed Speed of the robot in the y direction (sideways).
-   * @param rot Angular rate of the robot.
-   * @param fieldRelative Whether the provided x and y speeds are relative to the field.
+   * @param xSpeed        Speed of the robot in the x direction (forward).
+   * @param ySpeed        Speed of the robot in the y direction (sideways).
+   * @param rot           Angular rate of the robot.
+   * @param fieldRelative Whether the provided x and y speeds are relative to the
+   *                      field.
    */
   public void drive(
       double xSpeed, double ySpeed, double rot, boolean fieldRelative, double periodSeconds) {
-    var swerveModuleStates =
-        m_kinematics.toSwerveModuleStates(
-            ChassisSpeeds.discretize(
-                fieldRelative
-                    ? ChassisSpeeds.fromFieldRelativeSpeeds(
-                        xSpeed, ySpeed, rot, m_gyro.getRotation2d())
-                    : new ChassisSpeeds(xSpeed, ySpeed, rot),
-                periodSeconds));
+    var swerveModuleStates = m_kinematics.toSwerveModuleStates(
+        ChassisSpeeds.discretize(
+            fieldRelative
+                ? ChassisSpeeds.fromFieldRelativeSpeeds(
+                    xSpeed, ySpeed, rot, m_gyro.getRotation2d())
+                : new ChassisSpeeds(xSpeed, ySpeed, rot),
+            periodSeconds));
     SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, kMaxSpeed);
     m_frontLeft.setDesiredState(swerveModuleStates[0]);
     m_frontRight.setDesiredState(swerveModuleStates[1]);
@@ -77,10 +92,48 @@ public class Drivetrain {
     m_odometry.update(
         m_gyro.getRotation2d(),
         new SwerveModulePosition[] {
-          m_frontLeft.getPosition(),
-          m_frontRight.getPosition(),
-          m_backLeft.getPosition(),
-          m_backRight.getPosition()
+            m_frontLeft.getPosition(),
+            m_frontRight.getPosition(),
+            m_backLeft.getPosition(),
+            m_backRight.getPosition()
         });
   }
+
+  public void resetGyro() {
+    m_gyro.reset();
+  }
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("Swerve Module/fl angle", m_frontLeft.getAngle());
+    SmartDashboard.putNumber("Swerve Module/fr angle", m_frontRight.getAngle());
+    SmartDashboard.putNumber("Swerve Module/bl angle", m_backLeft.getAngle());
+    SmartDashboard.putNumber("Swerve Module/br angle", m_backRight.getAngle());
+
+    SmartDashboard.putNumber("Swerve Module/fl speed", m_frontLeft.getState().speedMetersPerSecond);
+    SmartDashboard.putNumber("Swerve Module/fr speed", m_frontRight.getState().speedMetersPerSecond);
+    SmartDashboard.putNumber("Swerve Module/bl speed", m_backLeft.getState().speedMetersPerSecond);
+    SmartDashboard.putNumber("Swerve Module/br speed", m_backRight.getState().speedMetersPerSecond);
+
+    SmartDashboard.putNumber("gyro", m_gyro.getAngle());
+
+    // do this later (maybe)
+
+    // SmartDashboard.putNumber("Swerve Module/fl desired speed",
+    // m_frontLeft.getState().speedMetersPerSecond);
+    // SmartDashboard.putNumber("Swerve Module/fr desired speed",
+    // m_frontRight.getState().speedMetersPerSecond);
+    // SmartDashboard.putNumber("Swerve Module/bl desired speed",
+    // m_backLeft.getState().speedMetersPerSecond);
+    // SmartDashboard.putNumber("Swerve Module/br desired speed",
+    // m_backRight.getState().speedMetersPerSecond);
+
+    // SmartDashboard.putData("Field", m_field);
+    
+    updateOdometry();
+    // m_field.setRobotPose(m_odometry.getPoseMeters());
+    super.periodic();
+    
+  }
+
 }
--- src/main/java/frc/robot/SwerveModule.java
+++ src/main/java/frc/robot/SwerveModule.java
@@ -5,6 +5,11 @@
 package frc.robot;
 
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.ctre.phoenix6.configs.MagnetSensorConfigs;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
@@ -16,15 +21,17 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class SwerveModule {
-  private static final double kWheelRadius = 0.0508;
-  private static final int kEncoderResolution = 4096;
+  private static final double WHEEL_RADIUS = Units.inchesToMeters(0.985);
+  private static final double WHEEL_CIRCUMFERENCE = WHEEL_RADIUS * 2 * Math.PI;
+  private static final double GEAR_RATIO = 7.2;
+  public static final double DRIVE_METERS_PER_MOTOR_ROTATION = WHEEL_RADIUS * Math.PI * 33 / 45 / 15/13;
 
   private static final double kModuleMaxAngularVelocity = Drivetrain.kMaxAngularSpeed;
-  private static final double kModuleMaxAngularAcceleration =
-      2 * Math.PI; // radians per second squared
+  private static final double kModuleMaxAngularAcceleration = 2 * Math.PI; // radians per second squared
 
   private final SparkMax m_driveMotor;
   private final SparkMax m_turningMotor;
@@ -33,63 +40,87 @@ public class SwerveModule {
   private final CANcoder m_turningEncoder;
 
   // Gains are for example purposes only - must be determined for your own robot!
-  private final PIDController m_drivePIDController = new PIDController(1, 0, 0);
+  private final PIDController m_drivePIDController = new PIDController(0.000, 0, 0);
 
-  // Gains are for example purposes only - must be determined for your own robot!
-  private final ProfiledPIDController m_turningPIDController =
-      new ProfiledPIDController(
-          1,
-          0,
-          0,
-          new TrapezoidProfile.Constraints(
-              kModuleMaxAngularVelocity, kModuleMaxAngularAcceleration));
+  private final PIDController m_turningPIDController = new PIDController(
+      0.02,
+      0,
+      0.0001);
 
   // Gains are for example purposes only - must be determined for your own robot!
   private final SimpleMotorFeedforward m_driveFeedforward = new SimpleMotorFeedforward(1, 3);
   private final SimpleMotorFeedforward m_turnFeedforward = new SimpleMotorFeedforward(1, 0.5);
 
   /**
-   * Constructs a SwerveModule with a drive motor, turning motor, drive encoder and turning encoder.
+   * Constructs a SwerveModule with a drive motor, turning motor, drive encoder
+   * and turning encoder.
    *
-   * @param driveMotorID PWM output for the drive motor.
-   * @param turningMotorID PWM output for the turning motor.
-   * @param driveEncoderID DIO input for the drive encoder channel A
-   * @param driveEncoderChannelB DIO input for the drive encoder channel B
-   * @param turningEncoderID DIO input for the turning encoder channel A
+   * @param driveMotorID           PWM output for the drive motor.
+   * @param turningMotorID         PWM output for the turning motor.
+   * @param driveEncoderID         DIO input for the drive encoder channel A
+   * @param driveEncoderChannelB   DIO input for the drive encoder channel B
+   * @param turningEncoderID       DIO input for the turning encoder channel A
    * @param turningEncoderChannelB DIO input for the turning encoder channel B
    */
   public SwerveModule(
       int driveMotorID,
       int turningMotorID,
-      int turningEncoderID) {
+      int turningEncoderID,
+      double turningOffset,
+      boolean driveInverted,
+      boolean turningInverted) {
     m_driveMotor = new SparkMax(driveMotorID, MotorType.kBrushless);
     m_turningMotor = new SparkMax(turningMotorID, MotorType.kBrushless);
 
     m_driveEncoder = m_driveMotor.getEncoder();
     m_turningEncoder = new CANcoder(turningEncoderID);
 
+    MagnetSensorConfigs magnetConfig = new MagnetSensorConfigs();
+    magnetConfig.MagnetOffset = turningOffset;
+    m_turningEncoder.getConfigurator().apply(magnetConfig);
+
+    SparkMaxConfig driveConfig = new SparkMaxConfig();
+    SparkMaxConfig turnConfig = new SparkMaxConfig();
+
+    driveConfig.encoder
+        .positionConversionFactor(DRIVE_METERS_PER_MOTOR_ROTATION);
+    driveConfig.encoder.velocityConversionFactor(
+        DRIVE_METERS_PER_MOTOR_ROTATION / 60);
+
+    driveConfig.idleMode(IdleMode.kBrake);
+
+    turnConfig.inverted(turningInverted);
+
+    m_driveMotor.configure(turnConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    m_turningMotor.configure(turnConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
     // Set the distance per pulse for the drive encoder. We can simply use the
     // distance traveled for one rotation of the wheel divided by the encoder
     // resolution.
-    //m_driveEncoder.setDistancePerPulse(2 * Math.PI * kWheelRadius / kEncoderResolution);
+    // m_driveEncoder.setDistancePerPulse(2 * Math.PI * kWheelRadius /
+    // kEncoderResolution);
 
-    // Set the distance (in this case, angle) in radians per pulse for the turning encoder.
+    // Set the distance (in this case, angle) in radians per pulse for the turning
+    // encoder.
     // This is the the angle through an entire rotation (2 * pi) divided by the
     // encoder resolution.
-    //m_turningEncoder.setDistancePerPulse(2 * Math.PI / kEncoderResolution);
+    // m_turningEncoder.setDistancePerPulse(2 * Math.PI / kEncoderResolution);
 
     // Limit the PID Controller's input range between -pi and pi and set the input
     // to be continuous.
-    m_turningPIDController.enableContinuousInput(-Math.PI, Math.PI);
+    m_turningPIDController.enableContinuousInput(-180, 180);
+    //m_turningPIDController.reset(getAngle());
   }
-/**
- * 
- * @return
- * angle, in degrees
- */
-  public double getAngle(){
+
+  /**
+   * 
+   * @return
+   *         angle, in degrees
+   */
+  public double getAngle() {
     return m_turningEncoder.getAbsolutePosition().getValueAsDouble() * 360;
   }
+
   /**
    * Returns the current state of the module.
    *
@@ -107,7 +138,7 @@ public class SwerveModule {
    */
   public SwerveModulePosition getPosition() {
     return new SwerveModulePosition(
-        m_driveEncoder.getPosition(),Rotation2d.fromDegrees(getAngle()));
+        m_driveEncoder.getPosition(), Rotation2d.fromDegrees(getAngle()));
   }
 
   /**
@@ -119,28 +150,33 @@ public class SwerveModule {
     var encoderRotation = Rotation2d.fromDegrees(getAngle());
 
     // Optimize the reference state to avoid spinning further than 90 degrees
-    desiredState.optimize(encoderRotation);
 
-    // Scale speed by cosine of angle error. This scales down movement perpendicular to the desired
-    // direction of travel that can occur when modules change directions. This results in smoother
+    // desiredState.optimize(encoderRotation);
+
+    // Scale speed by cosine of angle error. This scales down movement perpendicular
+    // to the desired
+    // direction of travel that can occur when modules change directions. This
+    // results in smoother
     // driving.
     desiredState.cosineScale(encoderRotation);
 
     // Calculate the drive output from the drive PID controller.
 
-    final double driveOutput =
-        m_drivePIDController.calculate(m_driveEncoder.getVelocity(), desiredState.speedMetersPerSecond);
+    final double driveOutput = m_drivePIDController.calculate(m_driveEncoder.getVelocity(),
+        desiredState.speedMetersPerSecond);
 
     final double driveFeedforward = m_driveFeedforward.calculate(desiredState.speedMetersPerSecond);
 
     // Calculate the turning motor output from the turning PID controller.
-    final double turnOutput =
-        m_turningPIDController.calculate(getAngle(), desiredState.angle.getDegrees());
+    final double turnOutput = m_turningPIDController.calculate(getAngle(), desiredState.angle.getDegrees());
 
-    final double turnFeedforward =
-        m_turnFeedforward.calculate(m_turningPIDController.getSetpoint().velocity);
+    //final double turnFeedforward = m_turnFeedforward.calculate(m_turningPIDController.getSetpoint().velocity);
 
     m_driveMotor.setVoltage(driveOutput + driveFeedforward);
-    m_turningMotor.setVoltage(turnOutput + turnFeedforward);
+    m_turningMotor.setVoltage(turnOutput);// + turnFeedforward
+
+    SmartDashboard.putNumber("Voltage", turnOutput);
+    SmartDashboard.putNumber("getAngle()", getAngle());
+    SmartDashboard.putNumber("getDegrees", desiredState.angle.getDegrees());
   }
 }
